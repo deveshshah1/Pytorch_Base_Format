70807a7a5694e9a23f8ca7498716f0b4c21445a9
Devesh Shah <deveshs@umich.edu>
2022-10-07 23:47:05 -0400

    create helperFxns.py and move some basic methods to there to hide implementation

---
+++ methods_and_networks/helperFxns.py
@@ -0,0 +1,61 @@
+"""
+Author: Devesh Shah
+Project Title: Pytorch Base Format
+
+This file holds all the suplementary helper functions we are currently using in the Run Manager / main loop. The
+functions in this file are separated here to create clarity when reading the other methods files.
+
+All the functions in this file start with the naming structure "helperFxn_..." to help recognize where the
+implementation of these methods can be found.
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def helperFxn_plot_all_accuracy(run_data):
+    """
+    This function plots the validation accuracy for all the runs across all the epochs onto one graph and returns
+    the figure. Additionally, this fxn finds the 5 highest validation accuracy's across all the runs and highlights
+    these runs on the graph to indicate the run number using a thicker line.
+
+    This fxn can easily be adapted to plot other properties between all runs and how many top K we would like to
+    emphasize.
+    :param run_data: A list structure of the results from every epoch for every run. Comes from the RunManager
+    variable self.run_data
+    :return: A figure with the validation accuracy of every run plotted on one graph. Top K val accuracy runs are
+    specially highlighted
+    """
+    df = pd.DataFrame.from_dict(run_data, orient='columns')
+    run_val_loss_dict = {}
+    run_list, max_val_acc_list = [], []
+
+    for index, row in df.iterrows():
+        run_number = row['run']
+        if run_number in run_val_loss_dict:
+            run_val_loss_dict[run_number].append(row['val_accuracy'])
+        else:
+            run_val_loss_dict[run_number] = [row['val_accuracy']]
+
+    for key, value in run_val_loss_dict.items():
+        max_val_acc_list.append(max(value))
+        run_list.append(key)
+
+    max_val_acc_list, run_list = (list(t) for t in zip(*sorted(zip(max_val_acc_list, run_list))))
+    run_list = run_list[-5:]
+
+    fig, ax = plt.subplots()
+    for key, value in run_val_loss_dict.items():
+        if key in run_list:
+            ax.plot(np.arange(1, len(value) + 1), value, label=key, linewidth=4)
+        else:
+            ax.plot(np.arange(1, len(value) + 1), value, linestyle='dahsed', label=key)
+
+    ax.set_ylabel('Validation_Accuracy')
+    ax.set_xlabel('Epoch')
+    pos = ax.get_position()
+    ax.set_position([pos.x0, pos.y0, pos.width *0.9, pos.height])
+    ax.legend(loc='center right', bbox_to_anchor=(1.25, 0.5))
+    return fig
--- methods_and_networks/runManager.py
+++ methods_and_networks/runManager.py
@@ -15,13 +15,11 @@ from torch.utils.tensorboard import SummaryWriter
 from collections import OrderedDict, namedtuple
 from itertools import product
 import time
-import pandas as pd
 from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plt
-import numpy as np
 import seaborn as sns
 import io
 from methods_and_networks.networks import *
+from methods_and_networks.helperFxns import *
 
 
 class NetworkFactory():
@@ -96,12 +94,12 @@ class RunManager():
 
         self.run_params = None
         self.run_names = {'Run_Number': [], 'Run_Name': []}
-        self.hyper_parameters = None
         self.run_count = 0
         self.run_data = []
         self.run_start_time = None
 
         self.network = None
+        self.hyper_parameters = None
         self.loader = None
         self.val_loader = None
         self.confusion_matrix = None
@@ -238,41 +236,8 @@ class RunManager():
             all_preds = torch.cat([all_preds, preds], dim=0)
         self.confusion_matrix = confusion_matrix(all_labels, all_preds)
 
-    def _plot_all_accuracy(self):
-        df = pd.DataFrame.from_dict(self.run_data, orient='columns')
-        run_val_loss_dict = {}
-        run_list, max_val_acc_list = [], []
-
-        for index, row in df.iterrows():
-            run_number = row['run']
-            if run_number in run_val_loss_dict:
-                run_val_loss_dict[run_number].append(row['val_accuracy'])
-            else:
-                run_val_loss_dict[run_number] = [row['val_accuracy']]
-
-        for key, value in run_val_loss_dict.items():
-            max_val_acc_list.append(max(value))
-            run_list.append(key)
-
-        max_val_acc_list, run_list = (list(t) for t in zip(*sorted(zip(max_val_acc_list, run_list))))
-        run_list = run_list[-5:]
-
-        fig, ax = plt.subplots()
-        for key, value in run_val_loss_dict.items():
-            if key in run_list:
-                ax.plot(np.arange(1, len(value) + 1), value, label=key, linewidth=4)
-            else:
-                ax.plot(np.arange(1, len(value) + 1), value, linestyle='dahsed', label=key)
-
-        ax.set_ylabel('Validation_Accuracy')
-        ax.set_xlabel('Epoch')
-        pos = ax.get_position()
-        ax.set_position([pos.x0, pos.y0, pos.width*0.9, pos.height])
-        ax.legend(loc='center right', bbox_to_anchor=(1.25, 0.5))
-        return fig
-
     def save(self, filename):
-        fig = self._plot_all_accuracy()
+        fig = helperFxn_plot_all_accuracy(self.run_data)
         writer = pd.ExcelWriter(filename, engine='xlsxwriter')
         df = pd.DataFrame.from_dict(self.run_data, orient='columns').to_excel(writer, sheet_name='All_Data')
         df2 = pd.DataFrame.from_dict(self.run_names, orient='columns').to_excel(writer, sheet_name='All_Plots')
